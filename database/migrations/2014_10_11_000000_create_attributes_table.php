7a86cc4a6f9cdbc28cdf2ea0e0eb8a73eb4d934d
abrahamuchos <agonzalez@wayuinc.com>
2023-05-08 18:33:40 -0400

    Add seed to Attribute

--- database/migrations/2014_10_11_000000_create_attributes_table.php
+++ database/migrations/2014_10_11_000000_create_attributes_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
                 ->onUpdate('cascade')
                 ->onDelete('cascade');
             $table->string('name', 255);
-            $table->string('value', 255);
+            $table->string('value', 255)->nullable();
         });
     }
 
